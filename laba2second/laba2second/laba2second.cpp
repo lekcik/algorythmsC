f1e839ef1037140b490148777b23c098590eaf3a
lekcik <cultcrib@gmail.com>
2024-09-27 23:39:20 +0200

    Funcs added

--- laba2second/laba2second/laba2second.cpp
+++ laba2second/laba2second/laba2second.cpp
@@ -1,6 +1,58 @@
 ﻿#include <iostream>
+#include <Windows.h>
+#include <time.h>
+
+int findSum(int array[][4], int rows, int columns) {
+    int sum = 0;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            if (array[i][j] == 1) {
+                sum += array[i][j];
+            }
+        }
+    }
+
+    return sum;
+}
+
+int countElems(int array[][4], int rows, int columns) {
+    int count = 0;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            if (array[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(static_cast<unsigned int>(time(0)));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int rows = 5;
+    const int columns = 4;
+
+    int array[rows][columns];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            array[i][j] = rand() % 6 - 3;
+            std::cout << array[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+
+    int sum = findSum(array, rows, columns);
+    int count = countElems(array, rows, columns);
+
+    std::cout << std::endl << "Сума елементів, рівних 1: " << sum << "\nКількість елементів, рівних 1: " << count << std::endl;
+
+    return 0;
 }
