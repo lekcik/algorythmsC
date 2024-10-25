06d5a852c5eeeda813858b72c04717384b9fbfa8
lekcik <cultcrib@gmail.com>
2024-10-25 18:50:50 +0200

    bubble sort added

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -2,12 +2,40 @@
 #include <Windows.h>
 #include <time.h>
 
+template<class T> void bubbleSort(T array[], int size) {
+    T temp;
+
+    for (int i = size; i >= 0; i--) {
+        for (int j = 0; j < i - 1; j++) {
+            if (array[j] < array[j + 1]) {
+                temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
     srand(static_cast<unsigned int>(time(0)));
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
+    int array[] = { 4, 18, 32, 2, 1, 19, 22, 40 };
+    int arraySize = sizeof(array) / sizeof(array[0]);
 
-    std::cout << "Hello World!\n";
+    std::cout << "UNORDERED ARRAY" << std::endl;
+    for (int& elem : array) {
+        std::cout << elem << " ";
+    }
+
+    bubbleSort(array, arraySize);
+
+    std::cout << "\n\nORDERED ARRAY" << std::endl;
+    for (int& elem : array) {
+        std::cout << elem << " ";
+    }
+
+    return 0;
 }
