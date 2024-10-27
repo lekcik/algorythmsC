2b88feb0189f90441a3349a017a0b5b5661b8a96
lekcik <cultcrib@gmail.com>
2024-10-27 23:11:57 +0100

    shall sort init

--- lab5/lab5/lab5.cpp
+++ lab5/lab5/lab5.cpp
@@ -5,7 +5,7 @@ template<class T> void insertionSort(T array[], int arraySize) {
     for (int i = 1; i < arraySize; i++) {
         T temp = array[i];
         for (int j = i-1; j >= 0; j--) {
-            if (array[j] > array[i]) {
+            if (array[j] >= temp) {
                 array[j + 1] = array[j];
             }
             else {
@@ -15,9 +15,13 @@ template<class T> void insertionSort(T array[], int arraySize) {
 
             if (j == 0) {
                 array[j] = temp;
-            }
+            }       
         }
     }    
+}
+
+template<class T> void shallSort(T array[], int arraySize) {
+
 }
 
 int main()
@@ -27,7 +31,34 @@ int main()
     int array[] = { 2, 5, 9, 1, 4, 7 };
     int arraySize = sizeof(array) / sizeof(array[0]);
 
+    std::cout << "Not ordered array" << std::endl;
     for (auto& elem : array) {
-
+        std::cout << elem << " ";
     }
+    std::cout << std::endl;
+
+    insertionSort(array, arraySize);
+
+    std::cout << "Ordered array with InsertionSort" << std::endl;
+    for (auto& elem : array) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n" << std::endl;
+
+    int secondArray[] = {2, 5, 9, 1, 4, 7};
+    int secondArraySize = sizeof(secondArray) / sizeof(secondArray[0]);
+
+    std::cout << "Not ordered array" << std::endl;
+    for (auto& elem : secondArray) {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+
+    //insertionSort(array, arraySize);
+
+    //std::cout << "Ordered array with InsertionSort" << std::endl;
+    //for (auto& elem : array) {
+    //    std::cout << elem << " ";
+    //}
+    //std::cout << std::endl;
 }
