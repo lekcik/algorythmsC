426fe100d25b570445954dbc5feb5e5d3a4b6f93
lekcik <cultcrib@gmail.com>
2024-10-27 09:08:31 +0100

    dynamically allocated array added

--- lab4/lab4/lab4.cpp
+++ lab4/lab4/lab4.cpp
@@ -8,26 +8,58 @@ int main()
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int rows;
-    int columns;
+    int n = 0, K = 0, L = 0, sum = 0;
+    int* firstArray;
 
-    std::cout << "Введіть кількість рядків: ";
-    std::cin >> rows;
-
-    std::cout << "Введіть кількість стовбців: ";
-    std::cin >> columns;
-
-    float** array = new float* [rows];
-
-    for (int i = 0; i < rows; i++) {
-        array[i] = new float[columns];
+    while (!(n > 0)) {
+        std::cout << "Enter arrays capacity (greater than 0): ";
+        std::cin >> n;
     }
 
-    for (int i = 0; i < rows; i++) {
-        delete[] array[i];
+    while (!((K >= 1) && (L > K) && (K < n) && (L < n))) {
+        std::cout << "Enter first limit (greater or equal to 1): ";
+        std::cin >> K;
+        std::cout << "Enter second limit (greater than first one): ";
+        std::cin >> L;
     }
-    delete[] array;
-    array = nullptr;
+
+    firstArray = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        firstArray[i] = rand() % 15;
+        std::cout << firstArray[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = K; i <= L; i++) {
+        sum += firstArray[i];
+    }
+
+    std::cout << "Sum of the elements from " << K << " to " << L << " is: " << sum << std::endl;
+
+    delete[] firstArray;
+    firstArray = nullptr;
+
+    //int rows;
+    //int columns;
+
+    //std::cout << "Введіть кількість рядків: ";
+    //std::cin >> rows;
+
+    //std::cout << "Введіть кількість стовбців: ";
+    //std::cin >> columns;
+
+    //float** array = new float* [rows];
+
+    //for (int i = 0; i < rows; i++) {
+    //    array[i] = new float[columns];
+    //}
+
+    //for (int i = 0; i < rows; i++) {
+    //    delete[] array[i];
+    //}
+    //delete[] array;
+    //array = nullptr;
 
     return 0;
 }
